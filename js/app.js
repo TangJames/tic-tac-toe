099552fb935bba908224d5499407dfe20cca23cf
James Tang <ulrichyu@outie.net>
2017-09-25 11:54:18 -0500

    ver5 condensed

--- js/app.js
+++ js/app.js
@@ -6,39 +6,30 @@ var boxes = document.querySelectorAll('.row .box');
 var playerOne = 'X';
 var moves = 0;
 
+//Winning game logic
+function winnerWinnerChickenDinner() {
+  var botRowAcross = win(boxes[6], boxes[7], boxes[8]);
+  var midRowAcross = win(boxes[3], boxes[4], boxes[5]);
+  var topRowAcross = win(boxes[0], boxes[1], boxes[2]);
+  var leftTop = win(boxes[0], boxes[3], boxes[6]);
+  var midTop = win(boxes[1], boxes[4], boxes[7]);
+  var rightTop = win(boxes[2], boxes[5], boxes[8]);
+  var leftDiag = win(boxes[0], boxes[4], boxes[8]);
+  var rightDiag = win(boxes[2], boxes[4], boxes[6]);
+
+  return topRowAcross || midRowAcross || botRowAcross || leftTop || midTop || rightTop || leftDiag || rightDiag;
+};
+//win condition
+function getWin() {
+  return winnerWinnerChickenDinner();
+}
+
 //define boxes and make win rule
 function win(box0, box1, box2) {
   return box0.textContent &&
          (box0.textContent === box1.textContent) &&
          (box1.textContent === box2.textContent);
 };
-//Winning game logic
-function acrossWin() {
-  var botRow = win(boxes[6], boxes[7], boxes[8]);
-  var midRow = win(boxes[3], boxes[4], boxes[5]);
-  var topRow = win(boxes[0], boxes[1], boxes[2]);
-
-  return topRow || midRow || botRow;
-};
-
-function verticalWin() {
-  var left = win(boxes[0], boxes[3], boxes[6]);
-  var mid = win(boxes[1], boxes[4], boxes[7]);
-  var right = win(boxes[2], boxes[5], boxes[8]);
-
-  return left || mid || right;
-};
-
-function diagWin() {
-  var leftDown = win(boxes[0], boxes[4], boxes[8]);
-  var rightDown = win(boxes[2], boxes[4], boxes[6]);
-
-  return leftDown || rightDown;
-};
-//win condition
-function getWin() {
-  return diagWin() || verticalWin() || acrossWin();
-}
 
 //Switch players back and forth
 function switchPlayer() {
