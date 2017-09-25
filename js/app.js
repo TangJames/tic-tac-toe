e35b0b3dbe7ca1553723516ff6932ac7c0b25b2f
James Tang <ulrichyu@outie.net>
2017-09-24 21:17:57 -0500

    finished product ver1

--- js/app.js
+++ js/app.js
@@ -1,44 +1,91 @@
 //loop through
 console.log('helloworld');
 
-var getBox = document.querySelectorAll('.row .box');
-var playerOne = 'O';
+//select rows and boxes and define playerOne
+var boxes = document.querySelectorAll('.row .box');
+var playerOne = 'X';
 var moves = 0;
 
-function threeInRow(firstBox, secondBox, thirdBox) {
-  firstBox = firstBox.textContent;
-  secondBox = secondBox.textContent;
-  thirdBox = thirdBox.textContent;
-
-if ((firstBox === secondBox) && (secondBox === thirdBox))
-  if (firstBox === 'X') {
-    return 'X';
-  } else if (firstBox === 'O') {
-    return 'O';
-  }
-}
-  return null;
+//define boxes and make rule
+function threeInRow(box0, box1, box2) {
+  return box0.textContent &&
+         (box0.textContent === box1.textContent) &&
+         (box1.textContent === box2.textContent);
 };
-
+//Winning game logic
 function acrossWin() {
-  var botRow = threeInRow(getBox[6], getBox[7], getBox[8]);
-  var midRow = threeInRow(getBox[3], getBox[4], getBox[5]);
-  var topRow = threeInRow(getBox[0], getBox[1], getBox[2]);
+  var botRow = threeInRow(boxes[6], boxes[7], boxes[8]);
+  var midRow = threeInRow(boxes[3], boxes[4], boxes[5]);
+  var topRow = threeInRow(boxes[0], boxes[1], boxes[2]);
 
-  return topRow || (midRow || botRow);
+  return topRow || midRow || botRow;
 };
 
 function verticalWin() {
-  var left = threeInRow(getBox[0], getBox[3], getBox[6]);
-  var mid = threeInRow(getBox[1], getBox[4], getBox[7]);
-  var right = threeInRow(getBox[2], getBox[5], getBox[8]);
+  var left = threeInRow(boxes[0], boxes[3], boxes[6]);
+  var mid = threeInRow(boxes[1], boxes[4], boxes[7]);
+  var right = threeInRow(boxes[2], boxes[5], boxes[8]);
 
-  return left || (mid || right);
+  return left || mid || right;
 };
 
 function diagWin() {
-  var leftDown = threeInRow(getBox[0], getBox[4], getBox[8]);
-  var rightDown = threeInRow(getBox[2], getBox[4], getBox[6]);
+  var leftDown = threeInRow(boxes[0], boxes[4], boxes[8]);
+  var rightDown = threeInRow(boxes[2], boxes[4], boxes[6]);
 
-  return leftdown || rightDown;
+  return leftDown || rightDown;
 };
+//win condition
+function getWin() {
+  return diagWin() || verticalWin() || acrossWin();
+}
+
+//Switch players back and forth
+function switchPlayer() {
+  if (playerOne === 'O') {
+    playerOne = 'X';
+  } else {
+    playerOne = 'O';
+  }
+};
+
+//reset the game
+function resetGame() {
+  boxes.forEach((e) => {
+    e.innerText = '';
+  });
+  //if (boxes.classList === 'O') {
+    //boxes.classList.remove('O');
+  //}
+  //else if (boxes.classList === 'X') {
+    //boxes.classList.remove('X');
+  //}
+  playerOne = 'X';
+  moves = 0;
+};
+
+var reset = document.querySelector('.btn');
+reset.addEventListener('click', function(e) {
+  resetGame();
+});
+
+//the game
+boxes.forEach(function(element) {
+  element.addEventListener('click', function(e) {
+    if (this.textContent !== '')
+      return;
+    this.innerText = playerOne;
+    //this.classList.add(playerOne);
+    moves += 1;
+    if (getWin()) {
+      alert(`Player ${playerOne} won!`);
+    }
+    else if (moves < 9) {
+      switchPlayer();
+    }
+    else {
+      alert('TIE GAME');
+      switchPlayer();
+    }
+  });
+});
